d03903db73f274ada963ed5469706fdf8945abd6
MinhLN <lenhatminh11a1@gmail.com>
2022-11-29 18:33:42 +0700

    setup cookie for production

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(
     secret: "secret",
     resave: false,
     saveUninitialized: true,
+    cookie: { httpOnly: process.env.NODE_ENV !== 'development', secure: process.env.NODE_ENV !== 'development', maxAge: 1000 * 60 * 60 * 48, sameSite:  process.env.NODE_ENV !== 'development' ? 'none' : 'lax' }
   })
 );
 app.use(passport.initialize());
