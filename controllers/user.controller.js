bbddd0d33d2402b8d4000cb81ec390c660ad6c74
Le Nhat Minh <lenhatminh11a1@gmail.com>
2022-11-29 14:34:56 +0700

    add log for req user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const { API_CODE_SUCCESS } = require("../constants");
 
 exports.getCurrentUser = (req, res, next) => {
+  console.log(req.user)
   return res.json({
     code: API_CODE_SUCCESS,
     message: "Success",
